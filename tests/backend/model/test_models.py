2f15963032daa4aded51b9f212138abb42284d6b
EfimNovikov <sawtery@gmail.com>
2023-08-11 16:10:35 +0300

    added tests for task durations model

--- tests/backend/model/test_models.py
+++ tests/backend/model/test_models.py
@@ -1,13 +1,14 @@
-from typing import List
-from unicodedata import category
 import pytest
+
+from datetime import date
+from sqlalchemy.exc import IntegrityError
 
 from database.connector import MySQL
 from sqlalchemy.orm.session import Session
-from src.backend.model import task_category
 from src.backend.model.category import Category
 from src.backend.model.task import Task
 from src.backend.model.task_category import TaskCategory
+from src.backend.model.task_time import TaskTime
 
 from tests.testdata import having_created_category, having_created_task
 
@@ -83,3 +84,43 @@ def test_should_create_task_multiple_categories(session: Session):
    
     assert task_category2.task_id == task.id
     assert task_category2.category_id == category2.id
+
+def test_should_correctly_add_durations_to_task(session: Session):
+    task = having_created_task()
+    duration1 = TaskTime(task_day=date.today(), task_id=task.id, duration=60)
+    duration2 = TaskTime(task_day=date(2023, 8, 10), task_id=task.id, duration=60)
+
+    task.durations.append(duration1)
+    task.durations.append(duration2)
+
+    session.add(task)
+    session.add(duration1)
+    session.add(duration2)
+    session.flush()
+
+    task_time: TaskTime = session.get(TaskTime, (date.today(), task.id))
+    task_time_another: TaskTime = session.get(TaskTime, (date(2023, 8, 10), task.id))
+
+    assert task_time != None and task_time_another != None
+
+    task_with_duration = session.get(Task, task.id)
+
+    task_id_equals = task_with_duration.durations[0].task_id == task.id
+    date_is_today = task_with_duration.durations[0].task_day == date.today()
+
+    assert duration1.duration == task_time.duration == 60
+    assert task_id_equals and date_is_today
+
+def test_should_fail_to_add_duration_same_day_same_task(session: Session):
+    with pytest.raises(IntegrityError, match='Duplicate entry'):
+        task = having_created_task()
+        duration1 = TaskTime(task_day=date.today(), task_id=task.id, duration=60)
+        duration2 = TaskTime(task_day=date.today(), task_id=task.id, duration=10)
+
+        task.durations.append(duration1)
+        task.durations.append(duration2)
+
+        session.add(task)
+        session.add(duration1)
+        session.add(duration2)
+        session.flush()
