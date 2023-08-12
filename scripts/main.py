d69c12facf47492277195ea2c025beb8dc659f4a
Tobias Boxma <tobias.boxma@gmail.com>
2023-08-12 11:28:46 +0200

    renamed main.py -> app.py Added call to scripts/main.py

--- scripts/main.py
+++ scripts/main.py
@@ -1,11 +1,17 @@
+from subprocess import call
+
 # Run project in production mode
 def prod():
     print ("Hello Prod!")
+    call(["python", "../src/app.py"])
+
 
 # Run project in dev mode
 def dev():
     migrate()
     print ("Hello Dev!")
+    call(["python", "../src/app.py"])
+
 
 # Run database migration
 def migrate():
