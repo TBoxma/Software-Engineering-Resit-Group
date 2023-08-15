76291b2da338dd6f53980c63e757234fdcd20ecb
Tobias Boxma <tobias.boxma@gmail.com>
2023-08-15 13:47:06 +0200

    added to help

--- src/frontend/cli.py
+++ src/frontend/cli.py
@@ -7,9 +7,12 @@ class CLI:
         "help:         get all possible commands as a list",
         "exit:         exit the program",
         "new task:     add a task",
+        "new category: add a category",
         "update task:  change task attributes",
         "del task:     delete a task",
         "del category: delete a category",
+        "show task:    show a category",
+        "show category:show a category",
         "exit:         exit the program"
     ])
     
