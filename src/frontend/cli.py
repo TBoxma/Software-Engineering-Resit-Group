0f9983098ffe0a9979e23f7a58ec0c7b4bb0bd4a
Tobias Boxma <tobias.boxma@gmail.com>
2023-08-15 12:13:07 +0200

    category add

--- src/frontend/cli.py
+++ src/frontend/cli.py
@@ -1,5 +1,6 @@
 import click
 from ..backend.api.task_api import TaskApi
+from ..backend.api.category_api import CategoryApi
 
 class CLI:
     helper_string = '\n'.join([
@@ -67,6 +68,41 @@ class CLI:
             categories = []
         TaskApi.add(given_attributes["name"], categories)
 
+    def handle_new_category(self, *args:str) -> None:
+
+        given_attributes= {}
+        given_attributes["name"] = "new category"
+
+        # List of attributes that the user needs to give:
+        needs={}
+        needs["name"] = True
+        needs["id"] = False
+        needs["categories"] = False
+
+        # See which attributes are already passed in the input, and update given_attributes and needs accordingly:
+        print(len(args[0]))
+        if(len(args[0])>0):
+            for unparsed_attribute in args[0]:
+                parsed_attribute = unparsed_attribute.split(':')
+                if (len(parsed_attribute) == 2):
+                    if(needs.get(parsed_attribute[0])):
+                        try:
+                            given_attributes[parsed_attribute[0]] = parsed_attribute[1]
+                        except KeyError:
+                            continue
+                        try:
+                            needs[parsed_attribute[0]] = False
+                        except KeyError:
+                            continue
+                        
+        #get the needs that haven't been set yet
+        needs_to_do = {key : val for key, val in needs.items() if val == True}
+        if(len(needs_to_do)>0):
+            for need in needs_to_do:
+                given_attributes[need] = click.prompt(need, type=str, default="")
+        
+        print("Creating new category with attributes", str(given_attributes))
+        CategoryApi.add(given_attributes["name"])
     
     def start(self):
         while True:
