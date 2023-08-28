1b8e7f79d1d69cb3b26e40a860caf3237a1a71cb
Tobias Boxma <tobias.boxma@gmail.com>
2023-08-28 11:16:59 +0200

    changed descriptions

--- src/frontend/cli_functions/add.py
+++ src/frontend/cli_functions/add.py
@@ -5,18 +5,17 @@ from src.frontend.cli_functions.function import Function
 
 
 class Add(Function):
-    main_description = ["add", "Add spent time to an existing task"]
-    today_description = ['add (task) (minutes)', "add an amount of minutes to a task"]
-    otherday_description = ['add (task) (minutes)', "add an amount of minutes to a task"]
-
+    main_description = ["add_time", "Add spent time to an existing task. For details, type 'help add_time {today, other_day}'"]
+    today_description = ['add_time (task) (minutes)', "add an amount of minutes to a task. 80 minutes can be formatted like '80', '1h20m', or '14:00-15:20'"]
+    otherday_description = ['add_time (task) (minutes) (date)', "add an amount of minutes to a task on a specific day"]
     #Get the description as a list of string tuples [[command, desc]]
     def get_description_precise(self, args:[str] = []) -> [[str,str]]:
         if len(args)==0:
             return [self.task_description, self.category_description]
         match args[0]:
-            case 'task':
+            case 'today':
                 return [self.today_description]
-            case 'category':
+            case 'other_day':
                 return [self.otherday_description]
             case _:
                 return [[args[0], "does not exist or cannot be called in this context"]]
@@ -24,3 +23,8 @@ class Add(Function):
     #Get the description as a single tuple [command, desc]
     def get_description_generic(self) -> [str,str]:
         return self.main_description
+    
+    def execute(self, args:[str] = []) -> None:
+        return
+
+
